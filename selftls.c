30b265a1b29e169d9d3824307c813a80cc48db06
Hanno <hanno@gentoo.org>
2016-02-13 20:53:04 +0100

    use indent -linux coding style

--- selftls.c
+++ selftls.c
@@ -13,12 +13,14 @@ To create self signed cert use:
 #include <stdlib.h>
 #include <stdio.h>
 
-void err() {
+void err()
+{
 	ERR_print_errors_fp(stderr);
 	exit(1);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 	int r, c, step;
 	unsigned char buf[4096];
 	char *ifi;
@@ -40,18 +42,24 @@ int main(int argc, char **argv) {
 	ERR_load_BIO_strings();
 	OpenSSL_add_all_algorithms();
 
-	if (!(sctx = SSL_CTX_new(TLSv1_method()))) err();
-	if (!SSL_CTX_use_certificate_file(sctx, "server.pem", SSL_FILETYPE_PEM)) err();
-	if (!SSL_CTX_use_PrivateKey_file(sctx, "server.key", SSL_FILETYPE_PEM)) err();
-	if (!(server = SSL_new(sctx))) err();
+	if (!(sctx = SSL_CTX_new(TLSv1_method())))
+		err();
+	if (!SSL_CTX_use_certificate_file(sctx, "server.pem", SSL_FILETYPE_PEM))
+		err();
+	if (!SSL_CTX_use_PrivateKey_file(sctx, "server.key", SSL_FILETYPE_PEM))
+		err();
+	if (!(server = SSL_new(sctx)))
+		err();
 
 	sinbio = BIO_new(BIO_s_mem());
 	soutbio = BIO_new(BIO_s_mem());
 	SSL_set_bio(server, sinbio, soutbio);
 	SSL_set_accept_state(server);
 
-	if(!(cctx = SSL_CTX_new(TLSv1_method()))) err();
-	if(!(client = SSL_new(cctx))) err();
+	if (!(cctx = SSL_CTX_new(TLSv1_method())))
+		err();
+	if (!(client = SSL_new(cctx)))
+		err();
 
 	cinbio = BIO_new(BIO_s_mem());
 	coutbio = BIO_new(BIO_s_mem());
@@ -63,13 +71,15 @@ int main(int argc, char **argv) {
 
 		r = SSL_do_handshake(client);
 		if (r == -1) {
-			if ((SSL_get_error(client, r) != SSL_ERROR_WANT_WRITE) &&
-				(SSL_get_error(client, r) != SSL_ERROR_WANT_READ))
+			if ((SSL_get_error(client, r) != SSL_ERROR_WANT_WRITE)
+			    && (SSL_get_error(client, r) !=
+				SSL_ERROR_WANT_READ))
 				err();
 		}
 
 		r = BIO_read(coutbio, buf, 4096);
-		if (r == -1) err();
+		if (r == -1)
+			err();
 		c++;
 
 		if (c == step) {
@@ -84,13 +94,15 @@ int main(int argc, char **argv) {
 
 		r = SSL_do_handshake(server);
 		if (r == -1) {
-			if ((SSL_get_error(server, r) != SSL_ERROR_WANT_WRITE) &&
-				(SSL_get_error(server, r) != SSL_ERROR_WANT_READ))
+			if ((SSL_get_error(server, r) != SSL_ERROR_WANT_WRITE)
+			    && (SSL_get_error(server, r) !=
+				SSL_ERROR_WANT_READ))
 				err();
 		}
 
 		r = BIO_read(soutbio, buf, 4096);
-		if (r == -1) err();
+		if (r == -1)
+			err();
 		c++;
 		if (c == step) {
 			f = fopen(ifi, "rb");
@@ -102,9 +114,12 @@ int main(int argc, char **argv) {
 		}
 		BIO_write(cinbio, buf, r);
 
-		printf("server state: %s / %s\n",SSL_state_string(server), SSL_state_string_long(server));
-		printf("client state: %s / %s\n",SSL_state_string(client), SSL_state_string_long(client));
-	} while ((!SSL_is_init_finished(server) || !SSL_is_init_finished(client)) && c<10);
+		printf("server state: %s / %s\n", SSL_state_string(server),
+		       SSL_state_string_long(server));
+		printf("client state: %s / %s\n", SSL_state_string(client),
+		       SSL_state_string_long(client));
+	} while ((!SSL_is_init_finished(server)
+		  || !SSL_is_init_finished(client)) && c < 10);
 
 	return 0;
 }
